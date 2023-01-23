ec46a1067373a5fde9713fa3cb544cb92432d3d1
DanielDDG <DanielGonzalezDDG@outlook.com>
2023-01-23 16:48:36 -0500

    Add colors to calculator

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+
+
 function add(a, b) {
     return a + b;
 }
@@ -13,4 +15,14 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+function percent(number) {
+    return number / 100;
+}
+
+function operate(num1, operator, num2) {
+
+}
+
+
 
