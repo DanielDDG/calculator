05c0f02b914e839acd3c851a3c9b4ebf0b44eeaf
DanielDDG <DanielGonzalezDDG@outlook.com>
2023-01-27 11:24:44 -0500

    Add full keyboard support

--- calculator.js
+++ calculator.js
@@ -1,12 +1,10 @@
-const box = document.querySelector('.box');
 const buttons = document.querySelectorAll('button');
-let displayValue = null;
+const box = document.querySelector('.box');
+let activeOperator = false;
+let activeSymbol = false;
 let operator = null;
 let num1 = 0;
 let num2 = 0;
-let activeOperator = false;
-let activeSymbol = false;
-box.textContent = displayValue;
 
 function add(a, b) {
     return parseInt(a) + parseInt(b);
@@ -69,9 +67,14 @@ function reset() {
     num2 = 0;
 }
 
+// Round answers with long decimals.
+// Display error message when dividing by zero.
+// Let users input and get decimals.
+// Add a backspace button that serves as C.
+// Add full keyboard support.
+
 buttonArray = Array.from(buttons);
 buttonArray.forEach(button => button.addEventListener('click', () => {
-
 
     if (button.textContent === 'AC') reset();
     else if (button.textContent === 'C') {box.textContent = box.textContent.slice(0, -1);} 
@@ -96,3 +99,27 @@ buttonArray.forEach(button => button.addEventListener('click', () => {
         }
     }
 }));
+
+document.addEventListener('keydown', (event) => {
+    let key = event.key;
+    console.log(key);
+    if (key === 'Delete') document.getElementById('reset').click();
+    if (key === 'Backspace') document.getElementById('delete').click();
+    if (key === 'Enter') document.getElementById('equals').click();
+    if (key === '%') document.getElementById('percent').click();
+    if (key === '/') document.getElementById('divide').click();
+    if (key === '*') document.getElementById('times').click();
+    if (key === '-') document.getElementById('minus').click();
+    if (key === '+') document.getElementById('plus').click();
+    if (key === '.') document.getElementById('dot').click();
+    if (key === '0') document.getElementById('0').click();
+    if (key === '1') document.getElementById('1').click();
+    if (key === '2') document.getElementById('2').click();
+    if (key === '3') document.getElementById('3').click();
+    if (key === '4') document.getElementById('4').click();
+    if (key === '5') document.getElementById('5').click();
+    if (key === '6') document.getElementById('6').click();
+    if (key === '7') document.getElementById('7').click();
+    if (key === '8') document.getElementById('8').click();
+    if (key === '9') document.getElementById('9').click();
+});
